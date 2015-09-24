a318e1e143c616ab591f64aff4c271ab6ed8ec1a
Leohongyi <wenhualiu.liu@gmail.com>
2015-09-23 23:40:26 -0400

    Initial Commit

---
+++ 网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻
+//
+//  Created by apple on 9/23/15.
+//  Copyright © 2015 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 网易新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  网易新闻
+//
+//  Created by apple on 9/23/15.
+//  Copyright © 2015 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
